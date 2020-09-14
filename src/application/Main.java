a5f6c54a40c6d01794a5f531635bd6465def48c3
Justin Joe <justin.aw.joe@gmail.com>
2020-09-14 22:40:19 +1200

    Added foundation for GUI with Gui class - base frame, board panel, side panel and basic menu bar implemented

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,17 @@
 package application;
 
+/**
+ * Main class for running Chap's Challenge
+ */
 public class Main {
+
+	/**
+	 * Key objects and components are invoked here to run the game.
+	 *
+	 * @param args the commandline arguments
+	 */
 	public static void main(String[] args) {
 		new TextGUI();
+		//TODO: Invoke Gui class here when it is ready
 	}
 }
