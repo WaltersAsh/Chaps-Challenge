2ad37ff1ea587bc88c447b3cc2bbfd37f46efb6e
Fangyi Yan <yanfang@myvuw.ac.nz>
2020-09-27 03:43:49 +1300

    empty constructor for Enemy.java
    JsonIgnore annotate for Enemy.java
    TODO: Need to call Enemy.initPathFinder after load from json

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Enemy.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Enemy.java
@@ -1,23 +1,36 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze.Direction;
 
 /**
  * The pathfinding enemy tokens
  *
  * @author Ian 300474717
- *
  */
 
 public class Enemy extends Movable {
   PathFinder pf;
 
-  public Enemy(String filename) {
-    super(filename, "EN");
+  public PathFinder getPf() {
+    return pf;
   }
 
+  public void setPf(PathFinder pf) {
+    this.pf = pf;
+  }
+
+  public Enemy() {
+  }
+
+  @JsonIgnore
+  //TODO: Need to call this function after load from json
   public void initPathFinder(Maze m) {
     pf = new PathFinder(m);
+  }
+
+  public Enemy(String filename) {
+    super(filename, "EN");
   }
 
   public Direction tickPathFinding() {
