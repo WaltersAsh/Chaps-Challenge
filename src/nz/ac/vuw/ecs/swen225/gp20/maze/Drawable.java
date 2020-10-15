665d2cb7bbce3f0bf5f68833ad4aa76e6a5a6135
Fangyi Yan <yanfang@myvuw.ac.nz>
2020-10-15 15:29:05 +1300

    minor refactor(equals and hashcode for Drawable.java)

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Drawable.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Drawable.java
@@ -11,7 +11,6 @@ import java.util.UUID;
  * Base class for anything which can be drawn on the board
  *
  * @author Ian 300474717
- *
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS)
@@ -26,17 +25,14 @@ public class Drawable {
   public Drawable(String filename, String initials) {
 
     this.initials = initials;
-//		image = toolkit.getImage(filename);
     this.filename = filename;
   }
 
-    public Drawable() {}
+  public Drawable() {}
 
-    public String getInitials() {
+  public String getInitials() {
     return initials;
   }
-
-//	public Image getImage(){return image;}
 
   public String getFilename() {
     return filename;
@@ -46,4 +42,24 @@ public class Drawable {
     filename = newFile;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Drawable drawable = (Drawable) o;
+
+    if (id != null ? !id.equals(drawable.id) : drawable.id != null) return false;
+    if (getInitials() != null ? !getInitials().equals(drawable.getInitials()) : drawable.getInitials() != null)
+      return false;
+    return getFilename() != null ? getFilename().equals(drawable.getFilename()) : drawable.getFilename() == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (getInitials() != null ? getInitials().hashCode() : 0);
+    result = 31 * result + (getFilename() != null ? getFilename().hashCode() : 0);
+    return result;
+  }
 }
